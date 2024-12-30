3d3c79c3fa35358620db6ca1b38c130d96226ea7
Zeno <144168045+Zeno79@users.noreply.github.com>
2024-12-30 17:21:41 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -6,8 +6,7 @@ class WZMLStyle:
     ST_BN1_URL = 'https://t.me/Netflix_Back_Up'
     ST_BN2_NAME = 'ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ'
     ST_BN2_URL = 'https://t.me/+ir9yAZBx0lZiMzJl'
-    ST_MSG = '''<i>This bot can mirror all your links|files|torrents to Google Drive or any rclone cloud or to telegram or to ddl servers.</i>
-<b>Type {help_command} to get a list of available commands</b>'''
+    ST_MSG = '''<i>I am Leech Bot Powered By @Netflix_Dual ⚡</b>'''
     ST_BOTPM = '''<i>Now, This bot will send all your files and links here. Start Using ...</i>'''
     ST_UNAUTH = '''<i>I am Leech Bot Powered By @Netflix_Dual ⚡</i>'''
     OWN_TOKEN_GENERATE = '''<b>Temporary Token is not yours!</b>\n\n<i>Kindly generate your own.</i>'''
